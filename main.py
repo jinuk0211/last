92d8323e04d03d733fcefc4a440a5731193fad26
jinuklee <150532431+jinuk0211@users.noreply.github.com>
2025-04-05 21:39:21 +0900

    2

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def run(args):
         dataset_name = args.dataset#"MathVista_MINI"
         dataset = build_dataset(dataset_name, **dataset_kwargs)
         print(f'전체 데이터셋 길이:{len(dataset.data)}')
-        dataset.data = dataset.data.iloc[:5]
+        dataset.data = dataset.data.iloc[:100]
         data_len = len(dataset.data)
     except Exception as e:
         print(f'File must be standardized json!\nError type:{e}\n')
@@ -102,7 +102,7 @@ def run(args):
             
             
     dataset.data = dataset.data.drop(columns=['image'])  
-    output_path = '/workspace/last/dataset1.xlsx'
+    output_path = '/workspace/last/dataset.xlsx'
     dataset.data.to_excel(output_path, index=False)
 
     judge_kwargs = {
